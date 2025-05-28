58750f1b2b0c78f32fe2c83343cea2e479a4340c
guyllaumedemers <guyllaume_demers@hotmail.fr>
2025-05-27 20:52:58 -0400

    adding actor declared in GameState/PlayerState in GetRep and remove console warnings. added comments about it cause Unreal mentioned Object Reference being automatically replicated but yet again... log warning

--- Plugins/AVVM/Source/AVVMGameplay/Private/AVVMGameState.cpp
+++ Plugins/AVVM/Source/AVVMGameplay/Private/AVVMGameState.cpp
@@ -31,6 +31,16 @@ void AAVVMGameState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLi
 {
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 
-	// @gdemers Replication of object references is automatically handled by Unreal Engine's replication system.
-	// no need to mark the AInfo with DOREPLIFETIME
+	// @gdemers https://dev.epicgames.com/documentation/en-us/unreal-engine/replicate-actor-properties-in-unreal-engine
+	// Replicate Object References
+	// Replication of object references is automatically handled by Unreal Engine's replication system.
+	// If you have a UObject property that is replicated, references to that object are sent over network connections
+	// as a FNetworkGUID that is assigned by the authoritative server. The server then notifies all connected clients of this assignment.
+
+	// @gdemers object reference should be automatically handled by Unreal system according to the above but our console logs complains about the object not
+	// being added for replication in this function.
+	DOREPLIFETIME(AAVVMGameState, PlayerDeaths);
+	DOREPLIFETIME(AAVVMGameState, PlayerKillstreaks);
+	DOREPLIFETIME(AAVVMGameState, CapturedObjectives);
+	DOREPLIFETIME(AAVVMGameState, DiscoveredArea);
 }
