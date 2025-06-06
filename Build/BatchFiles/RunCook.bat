ffc5b62f82aaa1a04d4920e59e663e0f5aee020c
guyllaumedemers <guyllaume_demers@hotmail.fr>
2025-06-06 08:11:05 -0400

    fixing issue with whitespace in RunCook batfile so the project name doesnt have a trailing whitespace

--- Build/BatchFiles/RunCook.bat
+++ Build/BatchFiles/RunCook.bat
@@ -12,15 +12,12 @@ set CookMaps=
 rem ## Parse batch file argument list.
 :GetParameterArguments
 set Delim=%~2
-if /i "%~1" == "-Project" (if not "%Delim:~0,1%" == "-" (set ProjectName=%~2 & shift) else (goto Error_BadArgumentPassing))
-if /i "%~1" == "-Platforms" (if not "%Delim:~0,1%" == "-" (set CookPlatforms=%~2 & shift) else (goto Error_BadArgumentPassing))
-if /i "%~1" == "-Maps" (if not "%Delim:~0,1%" == "-" (set CookMaps=%~2 & shift) else (goto Error_BadArgumentPassing))
+if /i "%~1" == "-Project" (if not "%Delim:~0,1%" == "-" ((set ProjectName=%Delim%) & shift) else (goto Error_BadArgumentPassing))
+if /i "%~1" == "-Platforms" (if not "%Delim:~0,1%" == "-" ((set CookPlatforms=%Delim%) & shift) else (goto Error_BadArgumentPassing))
+if /i "%~1" == "-Maps" (if not "%Delim:~0,1%" == "-" ((set CookMaps=%Delim%) & shift) else (goto Error_BadArgumentPassing))
 REM rem ## Shift the position of the Parameters list so 
 shift
 if not "%~1" == "" goto GetParameterArguments
-
-rem ## Verify the BatchFile location.
-if not exist "%~dp0..\..\Source" goto Error_BatchFileInWrongLocation
 
 rem ## batch file parameters
 echo %ProjectName% %CookPlatforms% %CookMaps%
@@ -36,23 +33,14 @@ goto Error_CheckAlternativePath
 :Error_BadArgumentPassing
 rem ## Output a blank line
 echo.
-echo RunCook ERROR: The Parameter passing arguments are invalid. Argument passing should be done as follow : ./RunCook.bat -Arg0 Value0 -Arg1 Value1. Exiting process...
+echo RunCook ERROR: The Parameter passing arguments are invalid. Argument passing should be provided like so: ./RunCook.bat -Arg0 Value0 -Arg1 Value1.
 echo.
-pause
-goto Exit
-
-:Error_BatchFileInWrongLocation
-rem ## Output a blank line
-echo.
-echo RunCook ERROR: The batch file does not appear to be located in the \$(ProjectName)\Build\BatchFiles directory. Process must be run from withing this location.
-echo.
-rem ## Suspend the process of the batch program and request user input for program closure.
 pause
 goto Exit
 
 :Error_CheckAlternativePath
 echo.
-echo RunCook ERROR: UnrealEditor.exe not found. Searching in alternative locations.
+echo RunCook ERROR: UnrealEditor.exe cannot be executed from "%ProjectName%\Engine" due to a invalid directory. Searching alternate locations.
 echo.
 rem ## fsutils fsinfo drives return a single line -> Drives: C:\ ... which is why we are using token to create an additional variable who's input the drive list.
 for /f "tokens=1,*" %%g in ('fsutil fsinfo drives') do (
@@ -63,8 +51,10 @@ for /f "tokens=1,*" %%g in ('fsutil fsinfo drives') do (
 	
 :Error_CheckIfShortcutExist
 echo.
-echo RunCook ERROR: UnrealEditor.exe not under Drive Path. Searching for possible Shortcut define at location.
+echo RunCook ERROR: UnrealEditor.exe cannot be executed from "Driver\Documents\UnrealEngine". Searching for Shortcut define in "%ProjectName%".
 echo.
+rem ## Verify the BatchFile location.
+if not exist "%~dp0..\..\Source" (goto Error_BatchFileInWrongLocation)
 rem ## Move directory to project ROOT.
 pushd "%~dp0..\..\"
 for %%g in (*.lnk) do (
@@ -72,12 +62,22 @@ for %%g in (*.lnk) do (
 	echo "%~dp0%%g"
 	for %%h in (%CookPlatforms%) do (
 		rem ## TODO check for UnrealEditor
+		echo "%%h"
 	)
 )
 
+:Error_BatchFileInWrongLocation
+rem ## Output a blank line
+echo.
+echo RunCook ERROR: The batch file does not appear to be located in the "%ProjectName%\Build\BatchFiles" directory. Process must be run from within this location.
+echo.
+rem ## Suspend the process of the batch program and request user input for program closure.
+pause
+goto Exit
+
 :Error_CannotExecuteRunCook
 echo.
-echo RunCook ERROR: Command cannot be executed properly. No shortcut available. Exiting process...
+echo RunCook ERROR: No shortcut .lnk available. 
 echo.
 pause
 goto Exit
