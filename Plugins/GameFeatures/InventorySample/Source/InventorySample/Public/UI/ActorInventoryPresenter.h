879f4a905519ea655e26f1d8c7fd7fd1bbd70492
guyllaumedemers <guyllaume_demers@hotmail.fr>
2025-06-14 13:41:30 -0400

    adding Toggle inventory ability boiler plate code

--- Plugins/GameFeatures/InventorySample/Source/InventorySample/Public/UI/ActorInventoryPresenter.h
+++ Plugins/GameFeatures/InventorySample/Source/InventorySample/Public/UI/ActorInventoryPresenter.h
@@ -17,35 +17,19 @@
 //LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 //OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //SOFTWARE.
+#pragma once
 
-using UnrealBuildTool;
+#include "CoreMinimal.h"
 
-public class InventorySample : ModuleRules
+#include "Archetypes/AVVMPresenter.h"
+
+#include "ActorInventoryPresenter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INVENTORYSAMPLE_API UActorInventoryPresenter : public UAVVMPresenter
 {
-	public InventorySample(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-
-		PublicDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"AVVM",
-				"AVVMGameplay",
-				"CommonUI",
-				"Core",
-				"CoreUObject",
-				"DataRegistry",
-				"Engine",
-				"GameplayAbilities",
-				"GameplayTags",
-				"IrisCore",
-			}
-		);
-
-		PrivateDependencyModuleNames.AddRange(
-			new string[]
-			{
-			}
-		);
-	}
-}
+	GENERATED_BODY()
+};
