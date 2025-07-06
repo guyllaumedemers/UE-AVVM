4dad0bad47c5480901883262683fa1cda455ccd0
guyllaumedemers <guyllaume_demers@hotmail.fr>
2025-07-06 18:54:56 -0400

    Revert "removing unecessary call to TryExecuteNext and returning false directly when required"
    This reverts commit daa4035a8dcf26c6e07aa26dd93117477cb0cc29.

--- Plugins/AVVM/Source/AVVMGameplay/Private/Resources/AVVMResourceManagerComponent.cpp
+++ Plugins/AVVM/Source/AVVMGameplay/Private/Resources/AVVMResourceManagerComponent.cpp
@@ -236,12 +236,14 @@ void UAVVMResourceManagerComponent::OnSoftObjectAcquired()
 	const AActor* Outer = OwningOuter.Get();
 	if (!ensureAlwaysMsgf(IsValid(Outer), TEXT("Invalid Outer!")))
 	{
+		QueueingMechanism.TryExecuteNextRequest();
 		return;
 	}
 
 	const bool bResult = UAVVMUtilityFunctionLibrary::DoesImplementNativeOrBlueprintInterface<IAVVMResourceProvider, UAVVMResourceProvider>(Outer);
 	if (!ensureAlwaysMsgf(bResult, TEXT("Outer doesn't implement the IAVVMResourceProvider interface!")))
 	{
+		QueueingMechanism.TryExecuteNextRequest();
 		return;
 	}
 
@@ -265,7 +267,7 @@ bool UAVVMResourceManagerComponent::OnProcessAdditionalResources(const TArray<FD
 	auto* DataRegistrySubsystem = UDataRegistrySubsystem::Get();
 	if (!IsValid(DataRegistrySubsystem))
 	{
-		return false;
+		return QueueingMechanism.TryExecuteNextRequest();
 	}
 
 	const bool bIsEmpty = PendingRegistriesId.IsEmpty();
