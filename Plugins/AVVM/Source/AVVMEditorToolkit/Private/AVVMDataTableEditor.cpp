0d2df75f013e42e235fe5ba4cd4734d795aea046
guyllaume demers <guyllaume_demers@hotmail.fr>
2026-03-01 09:07:06 -0500

    @gdemers : Playing with Linkage properties

--- Plugins/AVVM/Source/AVVMEditorToolkit/Private/AVVMDataTableEditor.cpp
+++ Plugins/AVVM/Source/AVVMEditorToolkit/Private/AVVMDataTableEditor.cpp
@@ -21,22 +21,53 @@
 #include "CoreMinimal.h"
 
 /**
- *	@gdemers Private namespace for reusable api specific to the AVVM DataTableEditor toolkit.
+ *	@gdemers This should work ? but somehow my compiler doesn't allow it as it warns me of a func body being already
+ *	defined to the call below when including in another translation unit... Not sure why ? nested function are still
+ *	internal linkage, so they shouldnt violate the ODR.
  */
-namespace NS_AVVMDataTableEditor
+// namespace NS_AVVMDataTableEditor
+// {
+// 	static void OpenRecentFiles()
+// 	{
+// 		UE_LOG(LogTemp, Log, TEXT("NS_AVVMDataTableEditor::OpenRecentFiles"));
+// 	}
+//
+// 	static void OpenFile()
+// 	{
+// 		UE_LOG(LogTemp, Log, TEXT("NS_AVVMDataTableEditor::OpenFile"));
+// 	}
+//
+// 	static void SaveAll()
+// 	{
+// 		UE_LOG(LogTemp, Log, TEXT("NS_AVVMDataTableEditor::SaveAll"));
+// 	}
+// }
+
+/**
+ *	@gdemers Private namespace for reusable api specific to the AVVM DataTableEditor toolkit.
+ *	
+ *	Note : Inline namespace expose the internal content to the parent scope, allowing me
+ *	to declare a name for accessing the nested content without affecting linkage properties.
+ */
+inline namespace NS_AVVMDataTableEditor
 {
-	static void OpenRecentFiles()
+	// @gdemers anonymous namespace provide support for implicit internal linkage (no static required),
+	// and expose to the parent scope.
+	namespace
 	{
-		UE_LOG(LogTemp, Log, TEXT("NS_AVVMDataTableEditor::OpenRecentFiles"));
-	}
+		void OpenRecentFiles()
+		{
+			UE_LOG(LogTemp, Log, TEXT("NS_AVVMDataTableEditor::OpenRecentFiles"));
+		}
 
-	static void OpenFile()
-	{
-		UE_LOG(LogTemp, Log, TEXT("NS_AVVMDataTableEditor::OpenFile"));
-	}
+		void OpenFile()
+		{
+			UE_LOG(LogTemp, Log, TEXT("NS_AVVMDataTableEditor::OpenFile"));
+		}
 
-	static void SaveAll()
-	{
-		UE_LOG(LogTemp, Log, TEXT("NS_AVVMDataTableEditor::SaveAll"));
+		void SaveAll()
+		{
+			UE_LOG(LogTemp, Log, TEXT("NS_AVVMDataTableEditor::SaveAll"));
+		}
 	}
 }
