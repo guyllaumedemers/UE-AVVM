5e35ba6d063d2d553f74c1ae01aa25adfd4430ad
guyllaume demers <guyllaume_demers@hotmail.fr>
2026-03-29 21:48:32 -0400

    @gdemers : Adding GameInstance subsystem to allow user requesting PSO compilation on level instances while they navigate menus, etc...

---
+++ Plugins/AVVM/Source/AVVMToolkit/Private/AVVMPSOPreloadManagerSubsystem.cpp
@@ -0,0 +1,77 @@
+//Copyright(c) 2025 gdemers
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files(the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions :
+//
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+#include "AVVMPSOPreloadManagerSubsystem.h"
+
+#include "Engine/AssetManager.h"
+#include "Engine/GameInstance.h"
+
+FAVVMPSOPreloadToken UAVVMPSOPreloadManagerSubsystem::Static_PreloadPSO(const UGameInstance* GameInstance,
+                                                                        const TArray<TSoftObjectPtr<UWorld>>& LevelInstances)
+{
+	auto* Subsystem = UAVVMPSOPreloadManagerSubsystem::Get(GameInstance);
+	if (IsValid(Subsystem))
+	{
+		const FAVVMPSOPreloadToken NewToken = Subsystem->PreloadPSO(LevelInstances);
+		return NewToken;
+	}
+
+	static FAVVMPSOPreloadToken Empty;
+	return Empty;
+}
+
+void UAVVMPSOPreloadManagerSubsystem::Static_UnloadPSOHandle(const UGameInstance* GameInstance,
+                                                             const FAVVMPSOPreloadToken& Token)
+{
+	auto* Subsystem = UAVVMPSOPreloadManagerSubsystem::Get(GameInstance);
+	if (IsValid(Subsystem))
+	{
+		Subsystem->UnloadPSOHandle(Token);
+	}
+}
+
+UAVVMPSOPreloadManagerSubsystem* UAVVMPSOPreloadManagerSubsystem::Get(const UGameInstance* GameInstance)
+{
+	return UGameInstance::GetSubsystem<UAVVMPSOPreloadManagerSubsystem>(GameInstance);
+}
+
+FAVVMPSOPreloadToken UAVVMPSOPreloadManagerSubsystem::PreloadPSO(const TArray<TSoftObjectPtr<UWorld>>& LevelInstances)
+{
+	TArray<FSoftObjectPath> OutPaths;
+	for (const TSoftObjectPtr<UWorld>& LevelInstance : LevelInstances)
+	{
+		OutPaths.Add(LevelInstance.ToSoftObjectPath());
+	}
+
+	const TSharedPtr<FStreamableHandle> NewHandle = UAssetManager::Get().LoadAssetList(OutPaths);
+	const FAVVMPSOPreloadToken NewToken = FAVVMPSOPreloadToken::Make();
+
+	PSOHandles.FindOrAdd(NewToken.UniqueId, NewHandle);
+
+	return NewToken;
+}
+
+void UAVVMPSOPreloadManagerSubsystem::UnloadPSOHandle(const FAVVMPSOPreloadToken& Token)
+{
+	const TSharedPtr<FStreamableHandle>* SearchResult = PSOHandles.Find(Token.UniqueId);
+	if ((SearchResult != nullptr) && SearchResult->IsValid())
+	{
+		(*SearchResult)->ReleaseHandle();
+	}
+}
---
+++ Plugins/AVVM/Source/AVVMToolkit/Public/AVVMPSOPreloadManagerSubsystem.h
@@ -0,0 +1,82 @@
+//Copyright(c) 2025 gdemers
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files(the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions :
+//
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Subsystems/GameInstanceSubsystem.h"
+
+#include "AVVMPSOPreloadManagerSubsystem.generated.h"
+
+struct FStreamableHandle;
+
+/**
+ *	Class description:
+ *
+ *	FAVVMPSOPreloadToken describe a unique identifier that increment only when default construct. Can be safely
+ *	passed by copy around.
+ */
+USTRUCT(BlueprintType)
+struct FAVVMPSOPreloadToken
+{
+	GENERATED_BODY()
+	
+	static FAVVMPSOPreloadToken Make()
+	{
+		static uint32 GlobalUniqueId = 0;
+
+		FAVVMPSOPreloadToken NewToken;
+		NewToken.UniqueId = ++GlobalUniqueId;
+
+		return NewToken;
+	}
+	
+	UPROPERTY(Transient, BlueprintReadOnly)
+	int32 UniqueId = INDEX_NONE;
+};
+
+/**
+ *	Class description:
+ *	
+ *	UAVVMPSOPreloadManagerSubsystem is a subsystem requesting PSO compilation on World target before client travel query.
+ *	
+ *	Note : It's expected that this task be executed in the background while the user navigate menus, etc...
+ */
+UCLASS()
+class AVVMTOOLKIT_API UAVVMPSOPreloadManagerSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	static FAVVMPSOPreloadToken Static_PreloadPSO(const UGameInstance* GameInstance,
+	                                              const TArray<TSoftObjectPtr<UWorld>>& LevelInstances);
+
+	UFUNCTION(BlueprintCallable)
+	static void Static_UnloadPSOHandle(const UGameInstance* GameInstance,
+	                                   const FAVVMPSOPreloadToken& Token);
+
+protected:
+	static UAVVMPSOPreloadManagerSubsystem* Get(const UGameInstance* GameInstance);
+	FAVVMPSOPreloadToken PreloadPSO(const TArray<TSoftObjectPtr<UWorld>>& LevelInstances);
+	void UnloadPSOHandle(const FAVVMPSOPreloadToken& Token);
+	
+	TMap<int32/*PSOPreload.UniqueId*/, TSharedPtr<FStreamableHandle>> PSOHandles;
+};
